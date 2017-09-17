9fc7bcfb48e4462a72b74ef0c5189502ff8064e8
Jessica-Koch <jlkoch@sbgtv.com>
2017-09-17 16:22:32 -0700

    add ability to see blog index

--- src/components/posts_index.jsx
+++ src/components/posts_index.jsx
@@ -1,9 +1,46 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
+import {Link} from 'react-router-dom';
 
 class PostsIndex extends Component {
+    componentDidMount() {
+        console.log(`props in post index component: ${this.props.fetchPosts}`)
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    {post.title}
+                </li>
+            )
+        })
+    }
+
     render() {
-        return <div>Posts Index</div>
+        console.log(this.props.posts)
+        return (
+            <div className="container">
+                <div className="text-xs-right text-right">
+                    <Link className="btn btn-outline-warning" to="/posts/new">
+                        Add a Post
+                    </Link>
+                </div>
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
+            </div>
+        )
     }
 }
 
-export default PostsIndex;
+const mapStateToProps = (state) => {
+    return {posts: state.posts}
+}
+
+// shortcut for map dispatch to props, connect handles the dispatch action
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
+// eslint-disable-next-line
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux';
-import {BrowserRouter, Route} from 'react-router-dom';
-import PostsIndex from './components/posts_index'
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import reducers from  './reducers'
+import promise from 'redux-promise';
+import PostsIndex from './components/posts_index'
+import PostsNew from './components/posts_new'
+// import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-  <BrowserRouter>
-  <div>
-    <Route path="/" component={PostsIndex}/>
-    </div>
-  </BrowserRouter>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
 </Provider>
   , document.getElementById('root')
 );
 
-registerServiceWorker();
+// registerServiceWorker();
