93ff803b46cfefdc2b73ee41149cb979f589b5ee
Jessica-Koch <jlkoch@sbgtv.com>
2017-09-17 16:43:23 -0700

    update error styling

--- src/components/posts_new.jsx
+++ src/components/posts_new.jsx
@@ -1,17 +1,23 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {Link} from 'react-router-dom';
 
 class PostsNew extends Component {
     renderField(field) {
+        const {meta: {touched, error}} = field;
+        const className=`form-group ${touched && error ? 'has-danger' : ''}`
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
-                    // className="form-control"
+                    className="form-control"
                     type="text"
                     {...field.input}
                 />
-                {field.meta.error}
+                <div className="text-help form-control-feedback">
+                    {touched ? error : ''}
+
+                </div>
             </div>
         )
     }
@@ -22,24 +28,28 @@ class PostsNew extends Component {
     render() {
         const {handleSubmit} = this.props;
         return (
-            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-                <Field
-                    name="title"
-                    label="Title"
-                    component={this.renderField}
-                />
-                <Field
-                    label="Categories"
-                    name="categories"
-                    component={this.renderField}
-                />
-                <Field
-                    label="Post Content"
-                    name="content"
-                    component={this.renderField}
-                />
-                <button type="submit" className="btn btn-outline-warning">Submit</button>
-            </form>
+            <div className="container">
+
+                <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                    <Field
+                        name="title"
+                        label="Title"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Categories"
+                        name="categories"
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Post Content"
+                        name="content"
+                        component={this.renderField}
+                    />
+                    <button type="submit" className="btn btn-outline-warning">Submit</button>
+                    <Link to="/" className="btn btn-outline-danger">Cancel</Link>
+                </form>
+        </div>
         )
     }
 }
